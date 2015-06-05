1b52aa0ed02b006eddcfc73743a0eeb931844f33
Frank Mueller <mue@tideland.biz>
2015-06-05 18:57:57 +0200

    Migrated the errors package.

---
+++ errors/doc.go
@@ -0,0 +1,46 @@
+// Tideland Go Library - Errors
+//
+// Copyright (C) 2013-2015 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+// The Tideland Go Library errors package allows to create more
+// detailed errors than with errors.New() or fmt.Errorf().  When trying
+// to differentiate between different errors or to carry helpful
+// payload own types are needed.
+//
+// The errors package allows to easily created formatted errors
+// with New() like with the fmt.Errorf() function, but also with an
+// error code. Additionlly a Messages instance has to be passed
+// to map the error code to their according messages.
+//
+// If an error alreay exists use Annotate(). This way the original
+// error will be stored and can be retrieved with Annotated(). Also
+// its error message will be appended to the created error separated
+// by a colon.
+//
+// All errors additionally contain their package, filename and line
+// number. These information can be retrieved using Location(). In
+// case of a chain of annotated errors those can be retrieved as a
+// slice of errors with Stack().
+package errors
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"github.com/tideland/golib/version"
+)
+
+//--------------------
+// VERSION
+//--------------------
+
+// PackageVersion returns the version of the version package.
+func PackageVersion() version.Version {
+	return version.New(4, 0, 0)
+}
+
+// EOF
---
+++ errors/errors_test.go
@@ -0,0 +1,85 @@
+// Tideland Go Library - Errors - Unit Tests
+//
+// Copyright (C) 2013-2015 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+package errors_test
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"testing"
+
+	"github.com/tideland/golib/errors"
+	"github.com/tideland/golib/audit"
+)
+
+//--------------------
+// TESTS
+//--------------------
+
+// Test the validation.
+func TestValidation(t *testing.T) {
+	assert := audit.NewTestingAssertion(t, true)
+
+	ec := 1
+	messages := errors.Messages{ec: "valid"}
+	err := errors.New(ec, messages)
+	packageName, fileName, line, lerr := errors.Location(err)
+
+	assert.True(errors.Valid(err))
+	assert.Nil(lerr)
+	assert.Equal(packageName, "github.com/tideland/golib/errors_test")
+	assert.Equal(fileName, "errors_test.go")
+	assert.Equal(line, 31)
+}
+
+// Test the annotation of errors.
+func TestAnnotation(t *testing.T) {
+	assert := audit.NewTestingAssertion(t, true)
+
+	ec := 123
+	messages := errors.Messages{ec: "annotated"}
+	aerr := testError("wrapped")
+	err := errors.Annotate(aerr, ec, messages)
+
+	assert.ErrorMatch(err, `\[ERRORS_TEST:123\] annotated: wrapped`)
+	assert.Equal(errors.Annotated(err), aerr)
+	assert.True(errors.IsInvalidTypeError(errors.Annotated(aerr)))
+	assert.Length(errors.Stack(err), 2)
+}
+
+// Test creation and checking.
+func TestIsError(t *testing.T) {
+	assert := audit.NewTestingAssertion(t, true)
+
+	ec := 42
+	messages := errors.Messages{ec: "test error %d"}
+	err := errors.New(ec, messages, 1)
+
+	assert.ErrorMatch(err, `\[ERRORS_TEST:042\] test error 1`)
+	assert.True(errors.IsError(err, ec))
+	assert.False(errors.IsError(err, 0))
+
+	err = testError("test error 2")
+
+	assert.ErrorMatch(err, "test error 2")
+	assert.False(errors.IsError(err, ec))
+	assert.False(errors.IsError(err, 0))
+}
+
+//--------------------
+// HELPERS
+//--------------------
+
+type testError string
+
+func (e testError) Error() string {
+	return string(e)
+}
+
+// EOF
