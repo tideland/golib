779678efaafc22a185d0f42acfd540ac6657d36d
Frank Mueller <mue@tideland.biz>
2015-06-10 23:38:55 +0200

    Fixed wrong comment.

--- collections/doc.go
+++ collections/doc.go
@@ -5,8 +5,8 @@
 // All rights reserved. Use of this source code is governed
 // by the new BSD license.
 
-// The Tideland GODM collections provide some typical and often used
-// collection types like stacks and directories. They are implemented
+// The Tideland Go Library collections package provides some typical and
+// often used collection types like stacks and trees. They are implemented
 // as generic collections managing empty interfaces as well as typed
 // ones, e.g. for strings. They are not synchronized, so this has to
 // be done by the user.
