8402c7d0ceb1b630fb14082aaadbe2bad4910187
Frank Mueller <mue@tideland.biz>
2015-06-26 23:22:40 +0200

    Better cell behaviors documentation.

--- cells/behaviors/doc.go
+++ cells/behaviors/doc.go
@@ -16,22 +16,31 @@
 //
 // - the broadcaster behavior simply emits all received events to all
 // subscribers;
+//
 // - the collector behavior collects all received events and also emits
 // them, they can be retrieved and resetted;
+//
 // - the counter behavior increments and emits counters identified by
 // the return value of a configurable function and the individual events,
 // the counters can be retrieved and resetted;
+//
 // - the filter behavior is created with a filtering function which is
 // called for each event, when it returns true the event is emitted;
+//
 // - the FSM behavior implements a finite state machine, state functions
 // process the events and return the following state;
+//
 // - the logger behavior logs every event at info level;
+//
 // - the mapper behavior is created with a mapping function processing
 // each event and returning a new mapped one;
+//
 // - the round robin behavior distributes each received event round robin
 // to its subscribers;
+//
 // - the scene behavior stores a received payload at the event topic as
 // key in the event scene, useful in testing scenarios;
+//
 // - the ticker behavior emits a tick event in a defined interval to its
 // subscribers.
 package behaviors
