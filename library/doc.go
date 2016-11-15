ced741039bac5c8a4e127e72a618868d13a27ba7
themue <mue@tideland.biz>
2016-11-15 14:48:14 +0100

    Added a library doc file for the next release

---
+++ library/doc.go
@@ -0,0 +1,12 @@
+// Tideland Go Library
+//
+// Copyright (C) 2012-2016 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+// The Tideland Go Library contains a larger set of useful Google
+// Go packages for different purposes.
+package library
+
+// EOF
