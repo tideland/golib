2c603f16943b574dec1d3c791834ad60703d3a5a
themue <mue@tideland.biz>
2016-11-15 15:02:36 +0100

    Changed wrong header dates

--- library/doc.go
+++ library/doc.go
@@ -1,6 +1,6 @@
 // Tideland Go Library
 //
-// Copyright (C) 2012-2016 Frank Mueller / Tideland / Oldenburg / Germany
+// Copyright (C) 2009-2016 Frank Mueller / Tideland / Oldenburg / Germany
 //
 // All rights reserved. Use of this source code is governed
 // by the new BSD license.
