c21aa8ee212b4c3b0672364ed30ba12417098be3
themue <mue@tideland.biz>
2017-03-16 15:24:49 +0100

    FileCacheable now returns io.ReadCloser
    In case of too large files the file will be directly used instead of a buffer.

--- cache/loader.go
+++ cache/loader.go
@@ -18,6 +18,8 @@ import (
 	"path/filepath"
 	"time"
 
+	"bytes"
+
 	"github.com/tideland/golib/errors"
 )
 
@@ -29,14 +31,33 @@ import (
 type FileCacheable interface {
 	Cacheable
 
-	io.Reader
+	// ReadCloser returns the io.ReadCloser for the
+	// cached file or the file itself if it's too large.
+	ReadCloser() (io.ReadCloser, error)
+}
+
+// fileBuffer encapsulates a bytes.Buffer as io.ReadCloser.
+type fileBuffer struct {
+	b *bytes.Buffer
+}
+
+// Read implements the io.ReadCloser interface.
+func (fb *fileBuffer) Read(p []byte) (int, error) {
+	return fb.b.Read(p)
+}
+
+// Close implements the io.ReadCloser interface.
+func (fb *fileBuffer) Close() error {
+	return nil
 }
 
 // fileCacheable implements the FileCacheable interface.
 type fileCacheable struct {
-	name    string
-	modTime time.Time
-	data    []byte
+	name     string
+	fullname string
+	tooLarge bool
+	modTime  time.Time
+	data     []byte
 }
 
 // ID implements the Cacheable interface.
@@ -46,7 +67,7 @@ func (c *fileCacheable) ID() string {
 
 // IsOutdated implements the Cacheable interface.
 func (c *fileCacheable) IsOutdated() (bool, error) {
-	fi, err := os.Stat(c.name)
+	fi, err := os.Stat(c.fullname)
 	if err != nil {
 		return false, errors.Annotate(err, ErrFileChecking, errorMessages)
 	}
@@ -61,10 +82,19 @@ func (c *fileCacheable) Discard() error {
 	return nil
 }
 
-// Read implements the Reader interface.
-func (c *fileCacheable) Read(p []byte) (int, error) {
-	n := copy(p, c.data)
-	return n, nil
+// ReadCloser implements the FileCacheable interface.
+func (c *fileCacheable) ReadCloser() (io.ReadCloser, error) {
+	// Check if the file has to be returned directly because
+	// it is too large.
+	if c.tooLarge {
+		f, err := os.Open(c.fullname)
+		if err != nil {
+			return nil, errors.Annotate(err, ErrFileLoading, errorMessages, c.name)
+		}
+		return f, nil
+	}
+	// It's a cached buffer, so return this.
+	return &fileBuffer{bytes.NewBuffer(c.data)}, nil
 }
 
 // NewFileLoader returns a CacheableLoader for files. It
@@ -77,16 +107,22 @@ func NewFileLoader(root string, maxSize int64) CacheableLoader {
 			return nil, errors.Annotate(err, ErrFileLoading, errorMessages, name)
 		}
 		if fi.Size() > maxSize {
-			return nil, errors.New(ErrFileSize, errorMessages, name)
+			return &fileCacheable{
+				name:     name,
+				fullname: fn,
+				tooLarge: true,
+				modTime:  fi.ModTime(),
+			}, nil
 		}
 		data, err := ioutil.ReadFile(fn)
 		if err != nil {
 			return nil, errors.Annotate(err, ErrFileLoading, errorMessages, name)
 		}
 		return &fileCacheable{
-			name:    name,
-			modTime: fi.ModTime(),
-			data:    data,
+			name:     name,
+			fullname: fn,
+			modTime:  fi.ModTime(),
+			data:     data,
 		}, nil
 	}
 }
--- cache/loader_test.go
+++ cache/loader_test.go
@@ -17,8 +17,11 @@ import (
 
 	"path/filepath"
 
+	"time"
+
 	"github.com/tideland/golib/audit"
 	"github.com/tideland/golib/cache"
+	"github.com/tideland/golib/monitoring"
 )
 
 //--------------------
@@ -31,23 +34,46 @@ func TestFileLoader(t *testing.T) {
 	td := audit.NewTempDir(assert)
 	defer td.Restore()
 
-	createFile(assert, td.String(), "fa", 1000)
-	createFile(assert, td.String(), "fb", 1500)
-	createFile(assert, td.String(), "fc", 1999)
-	createFile(assert, td.String(), "fd", 2000)
-	createFile(assert, td.String(), "fe", 4000)
+	createFile(assert, td.String(), "fa", 1)
+	createFile(assert, td.String(), "fb", 2)
+	createFile(assert, td.String(), "fc", 3)
+	createFile(assert, td.String(), "fd", 4)
+	createFile(assert, td.String(), "fe", 5)
 
-	loader := cache.NewFileLoader(td.String(), 2000)
-
-	ca, err := loader("fa")
-	assert.Nil(err)
-	assert.Equal(ca.ID(), "fa")
-	fca, ok := ca.(cache.FileCacheable)
-	assert.True(ok)
-	da := make([]byte, 3000)
-	n, err := fca.Read(da)
-	assert.Nil(err)
-	assert.Equal(n, 1000)
+	mega := 1024 * 1024
+	loader := cache.NewFileLoader(td.String(), int64(3*mega))
+	tests := []struct {
+		name string
+		size int
+	}{
+		{"fa", 1},
+		{"fb", 2},
+		{"fc", 3},
+		{"fd", 4},
+		{"fe", 5},
+	}
+	for i, test := range tests {
+		assert.Logf("test #%d: %s with size %d mb", i, test.name, test.size)
+		for j := 0; j < 10; j++ {
+			m := monitoring.BeginMeasuring(test.name)
+			c, err := loader(test.name)
+			assert.Nil(err)
+			assert.Equal(c.ID(), test.name)
+			fc, ok := c.(cache.FileCacheable)
+			assert.True(ok)
+			p := make([]byte, test.size*mega)
+			rc, err := fc.ReadCloser()
+			assert.Nil(err)
+			n, err := rc.Read(p)
+			assert.Nil(err)
+			assert.Equal(n, test.size*mega)
+			err = rc.Close()
+			assert.Nil(err)
+			m.EndMeasuring()
+		}
+	}
+	time.Sleep(5 * time.Second)
+	monitoring.MeasuringPointsPrintAll()
 }
 
 //--------------------
@@ -57,8 +83,9 @@ func TestFileLoader(t *testing.T) {
 // createFile creates a file for loader tests.
 func createFile(assert audit.Assertion, dir, name string, size int) string {
 	fn := filepath.Join(dir, name)
+	mega := 1024 * 1024
 	data := []byte{}
-	for i := 0; i < size; i++ {
+	for i := 0; i < size*mega; i++ {
 		data = append(data, 'X')
 	}
 	err := ioutil.WriteFile(fn, []byte(data), 0644)
