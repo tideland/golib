9a40048182a9f13f34ed094a29d3504527537ab4
Frank Mueller <mue@tideland.biz>
2017-04-29 11:35:33 +0200

    Continued node and value handling

--- gjp/gjp.go
+++ gjp/gjp.go
@@ -30,7 +30,7 @@ type Document interface {
 // document implements Document.
 type document struct {
 	separator string
-	root      node
+	root      noder
 }
 
 // Parse reads a raw document and returns it as
@@ -43,7 +43,7 @@ func Parse(data []byte, separator string) (Document, error) {
 	}
 	return &document{
 		separator: separator,
-		root:      buildTree(raw),
+		root:      rawToNoder(raw),
 	}, nil
 }
 
@@ -52,29 +52,36 @@ func (d *document) ValueAsString(path, dv string) string {
 	return ""
 }
 
-// resolvePath tries to resolve the given path and
-// returns the found object.
-func (d *document) resolvePath(node[string]interface{}, pathParts []string) (interface{}, error) {
-	return nil, nil
+//--------------------
+// NODE AND VALUE
+//--------------------
+
+// noder lets a value or node tell if it is a node.
+type noder interface {
+	// isNode checks if the value is a node and
+	// returns it type-safe. Otherwise nil and false
+	// are returned.
+	isNode() (node, bool)
 }
 
-//--------------------
-// HELPER
-//--------------------
-
-// value represents a JSON value but also may be a node
-// or array. 
+// value represents a JSON value.
 type value interface{}
 
-// isNode checks if the value is a node, which is a
+// isNode implements noder.
 func (v value) isNode() (node, bool) {
 	switch vt := v.(type) {
+	case node:
+		return vt, true
 	case map[string]interface{}:
-		return node(vt), true
+		n := node{}
+		for k, v := range vt {
+			n[k] = rawToNoder(v)
+		}
+		return n, true
 	case []interface{}:
 		n := node{}
 		for i, v := range vt {
-			n[strconv.Itoa(i)] = v
+			n[strconv.Itoa(i)] = rawToNoder(v)
 		}
 		return n, true
 	default:
@@ -83,12 +90,47 @@ func (v value) isNode() (node, bool) {
 }
 
 // node represents one JSON object or array.
-type node map[string]value
+type node map[string]noder
 
-// buildTree conerts the raw document into a
-// better accessable tree of nodes.
-func buildTree(raw value} node {
-	return nil
+// isNode implements noder.
+func (n node) isNode() (node, bool) {
+	return n, true
+}
+
+// at returns the noder at the given path or
+// nil and false.
+func (n node) at(path []string) (noder, bool) {
+	lp := len(path)
+	if lp == 0 {
+		// End of path.
+		return n, true
+	}
+	nr, ok := n[path[0]]
+	if !ok {
+		// Path part not found.
+		return nil, false
+	}
+	nn, ok := nr.isNode()
+	if ok {
+		// Continue recursively.
+		return nn.at(path[1:])
+	}
+	if lp > 1 {
+		// Reached value before end of path.
+		return nil, false
+	}
+	// We're done.
+	return nr, true
+}
+
+// rawToNoder conerts the raw interface into a
+// noder which may be a node or a value. 
+func rawToNoder(raw interface{}} noder {
+	v := value(raw)
+	if n, ok := v.isNode(); ok {
+		return n
+	}
+	return v
 }
 
 // EOF
